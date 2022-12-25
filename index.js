696c420cb3bf124ac52f789b0348798b45418afa
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-24 21:53:22 -0500

    added inquirer and test questions to terminal

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const inquirer = require('inquirer');
 
 
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'What his your Github username ?',
+        name: 'name'
+    },
+    {
+        type: "input",
+        message: 'What is your email address ?',
+        name: 'email'
+    },
+])
+.then((response) => {
+    console.log(response);
+})
